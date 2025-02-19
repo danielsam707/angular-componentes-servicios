5e5414a65d88d6aa4504351f945ac2b0601afc62
Daniel Arias <danielsam707@gmail.com>
2025-02-18 23:50:02 -0500

    Clase 4: Uso de Outputs | Se envia informacion desde el hijo al padre, tener en cuenta que el nombre del evento que se activa en el padre se debe llamar igual que el Ouput en el hijo

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -9,4 +9,15 @@ import { Component, Input } from '@angular/core';
 export class ImgComponent {
 
   @Input() img: string = 'valir init';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = '/images/dd.jpg'
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.log('Log hijo');
+    this.loaded.emit(this.img);
+  }
 }
